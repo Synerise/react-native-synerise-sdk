ab9492341685572f7d8345123dc5ad208f561258
Krzysztof Kurzawa <krzysztof.kurzawa@synerise.com>
2024-01-16 23:58:00 +0100

    Release of version 0.18.0

---
+++ lib/classes/models/Misc/SyneriseSource.d.ts
@@ -0,0 +1,10 @@
+declare enum SyneriseSource {
+    NotSpecified = "NOT_SPECIFIED",
+    SimplePush = "SIMPLE_PUSH",
+    Banner = "BANNER",
+    Walkthrough = "WALKTHROUGH",
+    InAppMessage = "IN_APP_MESSAGE"
+}
+declare function SyneriseSourceFromString(string: string): SyneriseSource;
+declare function SyneriseSourceToString(activity: SyneriseSource): string;
+export { SyneriseSource, SyneriseSourceFromString, SyneriseSourceToString };
