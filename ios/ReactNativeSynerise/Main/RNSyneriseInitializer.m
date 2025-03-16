783e54114b287b5f55b9d8b8d2935fb1cec7f992
Krzysztof Kurzawa <krzysztof.kurzawa@synerise.com>
2025-03-16 23:24:25 +0000

    Release of version 0.24.4

--- ios/ReactNativeSynerise/Main/RNSyneriseInitializer.m
+++ ios/ReactNativeSynerise/Main/RNSyneriseInitializer.m
@@ -8,7 +8,7 @@
 
 #import "RNSyneriseInitializer.h"
 
-NSString * const SNRSyneriseSDKPluginVersion = @"0.24.3";
+NSString * const SNRSyneriseSDKPluginVersion = @"0.24.4";
 
 @implementation RNSyneriseInitializer
 
