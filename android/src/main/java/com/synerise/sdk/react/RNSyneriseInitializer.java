a86db198ec9b56cb46335bdc928c7eafeb4c9403
Krzysztof Kurzawa <krzysztof.kurzawa@synerise.com>
2025-07-14 08:49:58 +0000

    Release of version 1.3.1

--- android/src/main/java/com/synerise/sdk/react/RNSyneriseInitializer.java
+++ android/src/main/java/com/synerise/sdk/react/RNSyneriseInitializer.java
@@ -18,7 +18,7 @@ public class RNSyneriseInitializer {
     public Boolean initialDoNotTrack;
     public static volatile boolean isInitialized = false;
 
-    public static final String SDK_PLUGIN_VERSION = "1.3.0";
+    public static final String SDK_PLUGIN_VERSION = "1.3.1";
 
     public void initialize(Application app) {
         if (isInitialized == false) {
